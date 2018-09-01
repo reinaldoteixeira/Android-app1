1a8c48f6aee2c77452ce606bc8c7f0cde9fde4a7
Reinaldo Teixeira <reinaldo_teixeira@outlook.com>
2018-08-31 21:32:15 -0300

    alteracao_2

--- app/src/main/java/com/example/android/app1/MainActivity.kt
+++ app/src/main/java/com/example/android/app1/MainActivity.kt
@@ -1,39 +1,46 @@
 package com.example.android.app1
 
+import android.widget.Toast
+import android.widget.EditText
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.widget.Button
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
 
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        var Resultado = findViewById<TextView>(R.id.resultado)
+        var Gasolina = findViewById<EditText>(R.id.gasolina)
+        var Alcool = findViewById<EditText>(R.id.alcool)
+
+        val btnCalcular = findViewById<Button>(R.id.btncalcular)
+        btnCalcular?.setOnClickListener {
+
+             val valor1 = Gasolina.text.toString().toDouble()
+             val valor2 = Alcool.text.toString().toDouble()
+
+             val valorTotal = valor1 * 0.7
+
+            if(valorTotal < valor2){
+                Resultado.text = "A Gasolina é Melhor"
+            }
+            else{
+
+                Resultado.text = "O Etanol é Melhor"
+
+            }
+
+            Toast.makeText(this@MainActivity, valorTotal.toString(), Toast.LENGTH_LONG).show()
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
